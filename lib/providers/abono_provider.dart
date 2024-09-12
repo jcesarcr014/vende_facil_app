e17dcb354702923f720a1ed049c6314a49120eaf
Max-500 <213497@ids.upchiapas.edu.mx>
2024-09-12 15:35:03 -0600

    Abono Provider

---
+++ lib/providers/abono_provider.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:vende_facil/models/models.dart';
+import 'package:vende_facil/providers/globals.dart' as globals;
+
+
+class AbonoProvider {
+  final baseUrl = globals.baseUrl;
+  final respuesta = Resultado();
+
+  Future<Resultado> obtenerAbono(String abonoID) async {
+    final url = Uri.parse('$baseUrl/apartado-abono/$abonoID');
+    try {
+      final response = await http.get(url, headers: {
+          'Authorization': 'Bearer ${sesion.token}',
+      });
+  
+      final decodedData = jsonDecode(response.body);
+
+      respuesta.mensaje = decodedData['msg'];
+
+      if(decodedData['status'] == 1) {
+        respuesta.status = 1;
+        abonoSeleccionado.saldoActual = decodedData['abono']['saldo_actual'];
+        abonoSeleccionado.saldoAnterior = decodedData['abono']['saldo_anterior'];
+        abonoSeleccionado.fechaAbono = decodedData['abono']['fecha_abono'];
+        abonoSeleccionado.cantidadEfectivo = decodedData['abono']['cantidad_efectivo'];
+        abonoSeleccionado.cantidadTarjeta = decodedData['abono']['cantidad_tarjeta'];
+      } else {
+        respuesta.status = 0;
+      }
+    } catch (e) {
+      respuesta.status = 0;
+      respuesta.mensaje = 'Error en la peticion. $e';
+    }
+
+    return respuesta;
+
+  }
+}
